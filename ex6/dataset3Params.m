214b71f973bc18d0a134633f0992a5f03dc07735
Russell Cheung <russell.cheung@nix-digital.com>
2016-01-14 17:35:58 +0800

    Complete assignment

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,8 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+min_err = 0;
 
+for i = 1:columns(steps)
+	loc_C = steps(i)
+	for j = 1:columns(steps)
+		loc_sigma = steps(j);
+		model = svmTrain(X, y, loc_C, @(x1, x2) gaussianKernel(x1, x2, loc_sigma));
+		pred = svmPredict(model, Xval);
+		err = mean(double(pred ~= yval));
+		if (i == 1 && j == 1)
+			min_err = err;
+			C = loc_C;
+			sigma = loc_sigma;
+		else
+			if (err < min_err)
+				min_err = err;
+				C = loc_C;
+				sigma = loc_sigma;
+			endif
+		endif
+endfor
 
 
 
